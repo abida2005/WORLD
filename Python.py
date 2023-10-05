d6f806c80bfbee9b46f9ba5aad12c62ba28479ee
abida2005 <147016396+abida2005@users.noreply.github.com>
2023-10-05 08:44:50 +0530

    Create Python.py
    WAP to print basic operations in python.

---
+++ Python.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter number 1:")
+num2=int(input("Enter number 2:")
+sum=num1+num2
+print("The sum is",sum)
+sub=num1-num2
+print("The sub is",sub)
+mul=num1*num2
+print("The product is",mul)
